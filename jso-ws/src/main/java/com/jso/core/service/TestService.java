7825d57feeaabe50b777dea98073bd739229a77d
JSO-Technologies <JSO.Technologies@gmail.com>
2014-09-09 23:21:55 +0200

    Initialisation

---
+++ jso-ws/src/main/java/com/jso/core/service/TestService.java
@@ -0,0 +1,21 @@
+package com.jso.core.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status.OK;
+
+@Path("test")
+public class TestService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/hello")
+	public Response hello() {
+
+		return Response.status(OK).entity("hello world").build();
+	}
+}
